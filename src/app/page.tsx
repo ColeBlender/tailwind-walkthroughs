eacd64ed3d79f9e2fae359d79d9482de7f5187b8
Cole Blender <coleblender@gmail.com>
2024-05-28 20:17:06 -0700

    change travel link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,6 +72,6 @@ const projects = [
   {
     name: "Travel",
     youtubeLink:
-      "https://www.youtube.com/watch?v=dt144O8BkT4&list=PLUyHhQn1KLAXK4qY0YIjGQPA5DI2zPWd6&index=2",
+      "https://www.youtube.com/watch?v=EWtaRfOiUZs&list=PLUyHhQn1KLAXK4qY0YIjGQPA5DI2zPWd6&index=3",
   },
 ];
