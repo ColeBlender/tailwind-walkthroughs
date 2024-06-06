dc057a98f5dcc9bc0fdc9d2805e8ce6c0ff4e42c
Cole Blender <coleblender@gmail.com>
2024-06-06 16:22:27 -0700

    update yt prof link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const links = {
   youTube:
     "https://www.youtube.com/playlist?list=PLUyHhQn1KLAXK4qY0YIjGQPA5DI2zPWd6",
-  youTubeProfile: "https://www.youtube.com/@coleblender",
+  youTubeProfile: "https://www.youtube.com/@coleblender?sub_confirmation=1",
   x: "https://twitter.com/coleblender",
   gitHub: "https://github.com/ColeBlender/tailwind-walkthroughs",
   gitHubProfile: "https://github.com/ColeBlender",
